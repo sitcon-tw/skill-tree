5e09a3cde4507c0218ab6eaa9a27c5f6992586e5
gnehs <jayuiop@gmail.com>
2023-07-01 20:06:16 +0800

    updated style

--- components/TreeRoot.tsx
+++ components/TreeRoot.tsx
@@ -9,7 +9,9 @@ interface TreeRootAttr {
 export default function TreeRoot({ root }: { root: TreeRootAttr }) {
   return (
     <div
-      className="absolute w-[min(11vw,calc(768px/100*11))] h-[min(11vw,calc(768px/100*11))] text-[min(1.75vw,calc(768px/100*1.75))] text-center text-white rounded-full bg-primary-300 bg-opacity-50 flex items-center justify-center translate-x-[-50%] translate-y-[-50%] p-[min(1vw,calc(768px/100*1))] leading-[1.2em]"
+      className={`absolute w-[min(11vw,calc(768px/100*11))] h-[min(11vw,calc(768px/100*11))] text-[min(1.75vw,calc(768px/100*1.75))] text-center text-white rounded-full translate-x-[-50%] translate-y-[-50%] leading-[1.2em] border-[min(0.5vw,calc(768px/100*0.5))] ${
+        root.top > 700 ? `border-[#E8DCA2]` : `border-[#B0FFE7]`
+      }`}
       style={{
         top: `${
           ((root.top + (root.top > 700 ? -150 : 0) + 150 / 2) / 2380) * 100
@@ -17,7 +19,18 @@ export default function TreeRoot({ root }: { root: TreeRootAttr }) {
         left: `${((root.left + 150 / 2) / 1450) * 100}%`,
       }}
     >
-      {root.name}
+      <div className="relative w-full h-full flex items-center justify-center p-[min(1vw,calc(768px/100*1))]">
+        {root.name}
+        <div
+          className={`absolute w-full h-full bottom-0 left-0 rounded-full ${
+            root.top > 700 ? `bg-[#B0FFE766]` : `bg-[#B0FFE766]`
+          }`}
+          style={{
+            mask: `linear-gradient(180deg, transparent 50%, #000 50%)`,
+            WebkitMask: `linear-gradient(180deg, transparent 50%, #000 50%)`,
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
