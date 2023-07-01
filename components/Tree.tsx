2afc8f395dae07e4c39cc0602ca5269632185395
gnehs <jayuiop@gmail.com>
2023-07-01 15:43:42 +0800

    added motion

--- components/Tree.tsx
+++ components/Tree.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, useTime, useTransform, easeInOut } from "framer-motion";
 export default function Tree() {
   const starTwinkle = {
     hidden: {
@@ -24,6 +24,20 @@ export default function Tree() {
     },
   };
 
+  const time = useTime();
+  const warpTime = useTransform(time, (t) => t % 8000);
+  const warpTime2 = useTransform(time, (t) => t % 5000);
+  const starOpacity1 = useTransform(warpTime, [0, 4000, 8000], [1, 0.2, 1]);
+  const starOpacity2 = useTransform(warpTime, [0, 4000, 8000], [0.2, 1, 0.2]);
+  const planetY = useTransform(warpTime, [0, 4000, 8000], [0, 25, 0], {
+    ease: easeInOut,
+  });
+  const satelliteY = useTransform(warpTime2, [0, 2500, 5000], [10, -5, 10], {
+    ease: easeInOut,
+  });
+  const satelliteScale = useTransform(warpTime2, [0, 2500, 5000], [1, 0.8, 1], {
+    ease: easeInOut,
+  });
   return (
     <motion.div
       className="w-full relative"
@@ -36,36 +50,34 @@ export default function Tree() {
         className="w-full border border-white user-select-none pointer-events-none"
         draggable="false"
       />
-      <img
+      <motion.img
         src="/imgs/tree/plants1.svg"
         className="absolute -top-[4.8%] -left-[13%] w-[39%] user-select-none pointer-events-none"
         draggable="false"
+        style={{ y: planetY }}
       />
       <img
         src="/imgs/tree/plants2.svg"
         className="absolute top-[25.7%] left-0 min-w-[106.7%] user-select-none pointer-events-none"
         draggable="false"
       />
-      <img
+      <motion.img
         src="/imgs/tree/satellite.svg"
         className="absolute top-[38.5%] left-[65.75%] w-[8.4%] user-select-none pointer-events-none"
         draggable="false"
+        style={{ y: satelliteY, scale: satelliteScale }}
       />
       <motion.img
         src="/imgs/tree/star1.svg"
         className="absolute top-[38.5%] left-[0] w-[104%] user-select-none pointer-events-none"
         draggable="false"
-        variants={starTwinkle}
-        initial="hidden"
-        animate="visible"
+        style={{ opacity: starOpacity2 }}
       />
       <motion.img
         src="/imgs/tree/star2.svg"
         className="absolute top-[0] left-[0] w-[104%] user-select-none pointer-events-none"
         draggable="false"
-        variants={starTwinkle}
-        initial="hidden"
-        animate="visible"
+        style={{ opacity: starOpacity1 }}
       />
       {/* 開源精神 */}
       <div className="absolute top-[42%] left-[50%] w-max -translate-x-[50%] -translate-y-[50%] text-[min(3vw,calc(768px/100*3))] text-[#102143] font-bold">
