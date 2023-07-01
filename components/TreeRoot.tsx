17a58455cdbbb47b3908ba848583f4398e04e4f6
gnehs <jayuiop@gmail.com>
2023-07-01 19:55:35 +0800

    updated style

--- components/TreeRoot.tsx
+++ components/TreeRoot.tsx
@@ -9,10 +9,10 @@ interface TreeRootAttr {
 export default function TreeRoot({ root }: { root: TreeRootAttr }) {
   return (
     <div
-      className="absolute w-[min(11vw,calc(768px/100*11))] h-[min(11vw,calc(768px/100*11))] text-[min(1.75vw,calc(768px/100*1.75))] text-center text-white rounded-full bg-primary-300 bg-opacity-50 flex items-center justify-center translate-x-[-50%] translate-y-[-50%] p-[min(1vw,calc(768px/100*1))] leading-[1em]"
+      className="absolute w-[min(11vw,calc(768px/100*11))] h-[min(11vw,calc(768px/100*11))] text-[min(1.75vw,calc(768px/100*1.75))] text-center text-white rounded-full bg-primary-300 bg-opacity-50 flex items-center justify-center translate-x-[-50%] translate-y-[-50%] p-[min(1vw,calc(768px/100*1))] leading-[1.2em]"
       style={{
         top: `${
-          ((root.top + (root.top > 700 ? -135 : 0) + 150 / 2) / 2380) * 100
+          ((root.top + (root.top > 700 ? -150 : 0) + 150 / 2) / 2380) * 100
         }%`,
         left: `${((root.left + 150 / 2) / 1450) * 100}%`,
       }}
