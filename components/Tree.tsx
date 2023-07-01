ca27055ea2425b3160549f15ee470fb23a073fc0
gnehs <jayuiop@gmail.com>
2023-07-01 11:57:56 +0800

    added draggable="false"

--- components/Tree.tsx
+++ components/Tree.tsx
@@ -34,22 +34,27 @@ export default function Tree() {
       <img
         src="/imgs/tree/bg.svg"
         className="w-full border border-white user-select-none pointer-events-none"
+        draggable="false"
       />
       <img
         src="/imgs/tree/plants1.svg"
         className="absolute -top-[60px] -left-[100px] w-[300px] user-select-none pointer-events-none"
+        draggable="false"
       />
       <img
         src="/imgs/tree/plants2.svg"
         className="absolute top-[320px] left-0 min-w-[820px] user-select-none pointer-events-none"
+        draggable="false"
       />
       <img
         src="/imgs/tree/satellite.svg"
         className="absolute top-[480px] left-[505px] w-[65px] user-select-none pointer-events-none"
+        draggable="false"
       />
       <motion.img
         src="/imgs/tree/star1.svg"
         className="absolute top-[480px] left-[0px] w-[800px] user-select-none pointer-events-none"
+        draggable="false"
         variants={starTwinkle}
         initial="hidden"
         animate="visible"
@@ -57,6 +62,7 @@ export default function Tree() {
       <motion.img
         src="/imgs/tree/star2.svg"
         className="absolute top-[0px] left-[0px] w-[800px] user-select-none pointer-events-none"
+        draggable="false"
         variants={starTwinkle}
         initial="hidden"
         animate="visible"
