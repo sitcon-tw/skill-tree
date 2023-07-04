793243397e28828d9be025a8f33eb95347fdeebd
gnehs <jayuiop@gmail.com>
2023-07-05 00:15:13 +0800

    updated scanner

--- app/(app)/scanner/page.tsx
+++ app/(app)/scanner/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+import { QrScanner } from "@yudiel/react-qr-scanner";
+import { useEffect, useState } from "react";
 export default function Scanner() {
-  return <h1 className="text-white">Scanner</h1>;
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  function handleScan(result: string | null) {
+    console.log(result);
+    let matched = result?.match(/\?id=(.+)&/);
+    if (matched) {
+      let id = matched[1];
+      alert(`id: ${id}`);
+    }
+  }
+  return mounted ? (
+    <QrScanner
+      onDecode={(result) => handleScan(result)}
+      onError={(error) => console.log(error?.message)}
+    />
+  ) : (
+    <p>loading...</p>
+  );
 }
