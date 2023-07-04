f1411473fb08471349ff74a40483437c4f5085e5
gnehs <gnehs@users.noreply.github.com>
2023-07-04 16:28:18 +0000

    deploy: f5660499d45d0b51a1aa4212b0636ab138a2f378

--- _next/static/chunks/app/(app)/scanner/page-627099e09368dd3b.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[85],{3891:function(e,n,r){Promise.resolve().then(r.bind(r,8968))},8968:function(e,n,r){"use strict";r.r(n),r.d(n,{default:function(){return s}});var t=r(9268),o=r(9332),u=r(6006);function s(){let[e,n]=(0,u.useState)(!1);return(0,u.useEffect)(()=>n(!0),[]),e?(0,t.jsx)(o.h,{onDecode:e=>(function(e){console.log(e);let n=null==e?void 0:e.match(/\?id=(.+)&/);if(n){let e=n[1];alert("id: ".concat(e))}})(e),onError:e=>console.log(null==e?void 0:e.message)}):(0,t.jsx)("p",{children:"loading..."})}},3177:function(e,n,r){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var t=r(6006),o=Symbol.for("react.element"),u=Symbol.for("react.fragment"),s=Object.prototype.hasOwnProperty,c=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function l(e,n,r){var t,u={},l=null,i=null;for(t in void 0!==r&&(l=""+r),void 0!==n.key&&(l=""+n.key),void 0!==n.ref&&(i=n.ref),n)s.call(n,t)&&!f.hasOwnProperty(t)&&(u[t]=n[t]);if(e&&e.defaultProps)for(t in n=e.defaultProps)void 0===u[t]&&(u[t]=n[t]);return{$$typeof:o,type:e,key:l,ref:i,props:u,_owner:c.current}}n.Fragment=u,n.jsx=l,n.jsxs=l},9268:function(e,n,r){"use strict";e.exports=r(3177)}},function(e){e.O(0,[344,253,769,744],function(){return e(e.s=3891)}),_N_E=e.O()}]);
